477c285a62272eae074a378f16c726646bd1a0f8
ColinsBlares <118037471+ColinsBlares@users.noreply.github.com>
2024-12-19 13:16:30 +0300

    Update fib_recursive.py

--- fib_recursive.py
+++ fib_recursive.py
@@ -1,12 +1,13 @@
 import time
 
 
-def fib(n):
-    if n == 0:
-        return 0
-    elif n == 1:
+def fib(n: int) -> int:
+    if n == 1 or n == 2:
         return 1
-    return fib(n - 1) + fib(n - 2)
+    a, b = 1, 1
+    for _ in range(3, n + 1):
+        a, b = b, a + b
+    return b
 
 
 n = 6
